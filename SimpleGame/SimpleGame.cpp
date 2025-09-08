83441d39fdf2b5285205c74d1e3aa5b9b34e0050
KSH <pakheae@nate.com>
2025-09-08 15:52:04 +0900

    add

--- SimpleGame/SimpleGame.cpp
+++ SimpleGame/SimpleGame.cpp
@@ -23,7 +23,7 @@ void RenderScene(void)
 	glClearColor(0.0f, 0.3f, 0.3f, 1.0f);
 
 	// Renderer Test
-	g_Renderer->DrawSolidRect(0, 0, 0, 80, 1, 1, 1, 1);
+	g_Renderer->DrawSolidRect(0, 0, 0, 4, 1, 0, 1, 1);
 
 	glutSwapBuffers();
 }
@@ -78,7 +78,7 @@ int main(int argc, char **argv)
 	glutIdleFunc(Idle);
 	glutKeyboardFunc(KeyInput);
 	glutMouseFunc(MouseInput);
-	glutSpecialFunc(SpecialKeyInput);*/
+	glutSpecialFunc(SpecialKeyInput);
 
 	glutMainLoop();
 
